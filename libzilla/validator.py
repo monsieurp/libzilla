49d30113379f5ddf3c3366483fae66492b1075a0
Patrice Clement <monsieurp@gentoo.org>
2016-07-14 13:08:01 +0000

    Rework and document validator.py.

--- libzilla/validator.py
+++ libzilla/validator.py
@@ -8,28 +8,32 @@ logger = logging.getLogger(__name__)
 
 """This module contains a sole class for validating an URL."""
 
+
 def validate_url(url):
+    """Validate an URL according to the Bugzilla REST API and how it is exposed
+    to the outside world. The Bugzilla REST API is described in the following
+    Wiki document: https://wiki.mozilla.org/Bugzilla:REST_API.
+    url: URL to validate."""
+
     if not url:
         raise LibZillaException('URL is empty!')
 
     outurl = urlparse(url)
     scheme, base, path = outurl[0:3]
-    if not scheme and not base and path:
-        base = path
-        path = ''
 
     if not scheme or not scheme.startswith('https'):
         logger.info("HTTPS scheme not detected!")
         logger.info("Defaulting scheme to 'https'.")
         scheme = 'https'
 
+    if not base and path:
+        base = path
+        path = ''
+
     if 'rest' not in path:
-        logger.info('URL must point to the Bugzilla REST API!')
+        logger.info('URL must point to the REST API!')
         logger.info("Defaulting path to 'rest'.")
         path = '/rest'
-
-    if not base or not path:
-        raise LibZillaException('Malformed URL! \"{0}\"'.format(url))
 
     valid_url = '%s://%s%s' % (scheme, base, path)
     logger.info('Validated URL: {0}'.format(valid_url))
