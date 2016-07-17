432ce9b26be37b363aafef6c2afe20c2b70c0840
Patrice Clement <monsieurp@gentoo.org>
2016-07-17 16:41:08 +0200

    Add a summary field.

--- libzilla/bug.py
+++ libzilla/bug.py
@@ -1,21 +1,25 @@
 class Bug:
     def __init__(self,
                  bug_number,
+                 summary=None,
                  comment=None,
                  resolution=None,
                  status=None):
 
         self.bug_number = bug_number
+        self.summary = summary
         self.resolution = resolution
         self.status = status
         self.comment = comment
 
     def __str__(self):
         return """Bug #: [%s]
+Summary: [%s]
 RESOLUTION: [%s]
 STATUS: [%s]
 Comment: %s""" % (
             self.bug_number,
+            self.summary,
             self.resolution,
             self.status,
             self.comment
